f5fcb0417c8e1a401b43b921d3411988f35e28d2
HArsH-ChiTToRa <hny.hrshchittora@gmail.com>
2023-06-17 00:25:29 +0530

    Query Selector

---
+++ domStarted/querySelector.js
@@ -0,0 +1,5 @@
+
+var item2 = document.querySelector('.list-group-item:nth-child(2)');
+item2.style.backgroundColor = "green";
+var item3 = document.querySelector('.list-group-item:nth-child(3)');
+item3.style.display = "none";
