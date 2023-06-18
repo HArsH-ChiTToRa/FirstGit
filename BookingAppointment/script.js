8fa8047c1f8fb903b48b73de1717de8bf03bcd07
HArsH-ChiTToRa <hny.hrshchittora@gmail.com>
2023-06-19 00:29:59 +0530

    let scale the app to more user task

--- BookingAppointment/script.js
+++ BookingAppointment/script.js
@@ -1,11 +1,25 @@
-const Name = (document.getElementById('name')).value;
-const Email = (document.getElementById('email')).value;
-console.log(Name);
-var form = document.getElementById('addForm');
-form.addEventListener('submit', storeDetail)
 
-function storeDetail(e){
+var Div = document.getElementById('dv');
+var form = document.getElementById('addForm');
+form.addEventListener('submit', store);
+
+function store(e){
     e.preventDefault();
-    localStorage.setItem(Name,Email)
+    var mail = (document.getElementById('email')).value
+    var myObj = {
+        Name: (document.getElementById('name')).value,
+        Email: (document.getElementById('email')).value,
+        Phone: (document.getElementById('phone')).value
+    }
+    var obj_serial = JSON.stringify(myObj);
+    localStorage.setItem(mail, obj_serial);
+
+    var li = document.createElement('li');
+    li.className = "list-group-item";
+    li.appendChild(document.createTextNode(myObj.Name+"-"+myObj.Email+"-"+myObj.Phone));
+    Div.appendChild(li);
 }
+
+
+
 
